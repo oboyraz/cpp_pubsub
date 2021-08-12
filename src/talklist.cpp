14dfad6b0e9c9db6ee5d2d13841dc82be67f528c
oboyraz <oboyraz@sakarya.edu.tr>
2021-08-12 12:05:05 +0300

    First Commit

---
+++ src/talklist.cpp
@@ -0,0 +1,54 @@
+#include <chrono>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+using std::placeholders::_1;
+
+class MyPubSub : public rclcpp::Node
+
+{
+public:
+  MyPubSub()
+  : Node("Mypubsub"), count_(0)
+  {
+    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+
+    timer_ = this->create_wall_timer(
+      500ms, std::bind(&MyPubSub::timer_callback, this));
+
+    subscription_  = this->create_subscription<std_msgs::msg::String>(
+      "topic", 10, std::bind(&MyPubSub::topic_callback, this, _1));
+
+  }
+
+private:
+  void timer_callback()
+  {
+    auto message = std_msgs::msg::String();
+    message.data = "Hello, world! " + std::to_string(count_++);
+    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    publisher_->publish(message);
+  }
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  size_t count_;
+
+
+  void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+  {
+    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+  }
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MyPubSub>());
+  rclcpp::shutdown();
+  
+
+}
